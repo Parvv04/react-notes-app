027b7dfb64b214704deb2d7770d26b0d8c5041c2
Parvv04 <parzzabeth12@gmail.com>
2026-07-01 09:45:30 +0530

    added components

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,66 @@
 import { useState } from 'react'
 import './App.css'
 
+function NoteInput({title, setTitle, desc, setDesc}) {
+  return (
+    <div className='note-input'>
+      <input 
+        placeholder='Enter Title'
+        value= {title}
+        onChange= {(event) =>
+          setTitle(event.target.value)
+        }
+      />
+      <input
+        placeholder='Enter Description'
+        value = {desc}
+        onChange={(event) =>
+          setDesc(event.target.value)
+        }
+      />
+      
+    </div>
+  )
+}
+
+function Notes({notes, deleteNote}) {
+  return (
+    <div>
+      {
+        notes.length === 0 ?
+        <p>No notes to display</p>
+        :
+        (
+          <ul>
+            {notes.map((note) => (
+              <li key = {note.id}>
+                <h3>{note.title}</h3>
+                <p>{note.desc}</p>
+                <DeleteNote deleteNote={() => deleteNote(note.id)} />
+              </li>
+            ))}
+          </ul>
+        )
+      }
+    </div>
+  )
+}
+
+function DeleteNote({deleteNote}) {
+  return (
+      <button onClick={deleteNote}>
+        Delete Note
+      </button>
+  )
+}
+
 function App() {
   const [title,setTitle] = useState("");
   const [desc, setDesc] = useState("");
   const [notes, setNotes] = useState([]);
   
   function addNotes() {
-    if(title.trim() == "" || desc.trim() == ""){
+    if(title.trim() === "" || desc.trim() === ""){
       setTitle("");
       setDesc("");
       return;
@@ -24,48 +77,27 @@ function App() {
   }
 
   function deleteNote(id){
-    setNotes(notes.filter((task) =>
-      task.id !== id
+    setNotes(notes.filter((note) =>
+      note.id !== id
     ));
   }
 
   return (
     <div className="container">
-      <input 
-        placeholder='Enter Title'
-        value= {title}
-        onChange= {(event) =>
-          setTitle(event.target.value)
-        }
-      />
-      <input
-        placeholder='Enter Description'
-        value = {desc}
-        onChange={(event) =>
-          setDesc(event.target.value)
-        }
+      <h1>Notes App</h1>
+      <NoteInput 
+        title = {title}
+        setTitle = {setTitle}
+        desc = {desc}
+        setDesc = {setDesc}
       />
       <button onClick={addNotes}>
         Add Note
       </button>
-      {
-        notes.length === 0 ?
-        <p>No notes to display</p>
-        :
-        (
-          <ul>
-            {notes.map((note) => (
-              <li key = {note.id}>
-                {note.title}
-                <p>{note.desc}</p>
-                <button onClick={ () => {deleteNote(note.id)}}>
-                  Delete Note
-                </button>
-              </li>
-            ))}
-          </ul>
-        )
-      }
+      <Notes 
+        notes = {notes}
+        deleteNote = {deleteNote}
+      />
     </div>
   )
 }
